f6838d3e7c00b92496de0a3b386c103384f23902
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:31:39 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"___ she ___ to work now?",
+        options:["Are / going", "Is / going", "Does / go", "Is / goes"],
+        answer: 1
+    },
+    {
+     q:"___ you ___ speaking right now?",
+        options:["Are / speaking", "Are / speak", "Do/speak", "Is / speaking"],
+        answer: 0   
+    },
+    {
+        q:"___ they ___ computer games at the moment?",
+        options:["Is / playing", "Do / play", "Are / playing", "Are / play"],
+        answer: 2
+    },
+    {
+        q:"What ___ you ___ right now?",
+        options:["is / doing", "do / do", "- / doing", "are / doing"],
+        answer: 3
+    },
+    {
+        q:"___ your dad ___ dinner in the kitchen now?",
+        options:["Does / have", "Is / has", "Is / having", "Are / having"],
+        answer: 2
+    },
+    {
+        q:"What ___ he ___ at the moment?",
+        options:["does / write", "is / writing", "write", "are / writing"],
+        answer: 1
+    },
+    {
+        q:" 'Where ___ you ___ ?' 'I am going to the shop.'",
+        options:["are / going", "do / go", "am / going", "are / go"],
+        answer: 0
+    },
+    {
+        q:"___ your friend ___ to school today?",
+        options:["Is / go", "Does / going", "Are / going", "Is / going"],
+        answer: 3
+    },
+    {
+        q:"What ___ you ___ ?",
+        options:["are / buy", "are / buying", "is / buying", "do / buys"],
+        answer: 1
+    },
+    {
+        q:"Why ___ they ___ ?",
+        options:["is / laughing", "do / laughing", "are / laughing", "are / laugh"],
+        answer: 2
+    }
+
+]
